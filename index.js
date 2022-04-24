4b02d0934d8f40fdc56f0c398111d9eb0035635a
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-04-24 14:56:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// Write your code in this f
+const currentUser = "BINAMIN"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
